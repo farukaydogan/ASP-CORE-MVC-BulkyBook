a0d7ae57ff50830945e1265dba9cfc7731f80d4b
farukaydogan <faruk034595@gmail.com>
2023-11-05 17:41:59 +0300

    add migration and  default connection runned and worked update-database has works

---
+++ BulkyBook2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BulkyBook2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook2.Data
+{
+	public class ApplicationDbContext: DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+		public DbSet<Category> Categories { get; set; }
+	}
+}
+
--- BulkyBook2/Program.cs
+++ BulkyBook2/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace BulkyBook2;
+﻿using BulkyBook2.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook2;
 
 public class Program
 {
@@ -8,6 +11,10 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
+        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+            ));
+        builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
         var app = builder.Build();
 
